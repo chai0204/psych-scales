06adcfbb93dd22bf63ef5cc52e9b5b964e5f1095
Shun <junxiaosong508@gmail.com>
2026-04-10 21:22:59 +0900

    feat: ECR-GO（一般アタッチメントスタイル）とBSCS-J（セルフコントロール短縮版）を追加

--- lib/scales/index.ts
+++ lib/scales/index.ts
@@ -7,11 +7,15 @@ import type { Scale } from "../types";
 import bigFiveS from "./big-five-s.json";
 import bfi2jStudent from "./bfi-2-j-student.json";
 import bfi2jCommunity from "./bfi-2-j-community.json";
+import ecrGo from "./ecr-go.json";
+import bscsJ from "./bscs-j.json";
 
 const scaleList: Scale[] = [
   bigFiveS as Scale,
   bfi2jStudent as Scale,
   bfi2jCommunity as Scale,
+  ecrGo as Scale,
+  bscsJ as Scale,
 ];
 
 export function getAllScales(): Scale[] {
